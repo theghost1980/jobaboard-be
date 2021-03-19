33a542beee5233a11b2eccface781c7315dd2fbc
Saturno Mangieri <saturnob612@gmail.com>
2021-03-19 07:46:29 -0400

    adding PR on jobs

--- Publiccontroller.js
+++ Publiccontroller.js
@@ -5,6 +5,7 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 var User = require('./User');
 var Logs = require('./Logs');
+var Jobs = require('./Job');
 var jwt = require('jsonwebtoken');
 var config = require('./config');
 const time = new Date();
@@ -29,7 +30,25 @@ router.get('/getField', function(req, res){
         });
     }
 })
-//////END Public
+
+//////Public routes for user
+router.get('/getActiveJobs', function(req, res){
+    const username = req.headers['username'];
+    if(config.testingData){ console.log('To find:', username)};
+    if(!username || username === ""){
+        if(config.testingData){console.log('Empty query from public users req');}
+        return res.status(404).send({ status: 'failed', message: 'I cannot query emptiness. PR.'});
+    }else{
+        Job.find({ username: username, active: true },function(err,found){
+            if(err){
+                if(config.testingData){ console.log('Error getting jobs from user, public request.',err)};
+                return res.status(500).send({ error: err });
+            }
+            if(config.testingData){ console.log('Found from public request on Jobs',found)};
+            return res.status(200).send({ status: 'sucess', result: found});
+        });
+    }
+})
 ///////////////////////////////////////////////////////////////////////
 
 

228822ad976d7c1423ae173ff9eafd657057304e
Saturno Mangieri <saturnob612@gmail.com>
2021-03-19 06:25:46 -0400

    adding pub route for users

--- UserController.js
+++ UserController.js
@@ -363,6 +363,30 @@ router.get('/logs', function(req, res){
 });
 ///////////////////////////////////////////////////////////////////////
 
+
+///////////////////////////////////////////////////////////////////////
+//////Public routes for user
+router.get('/getField', function(req, res){
+    const query = req.headers['query'];
+    const jsonFields = JSON.parse(query);
+    if(config.testingData){ console.log('To find:', jsonFields)};
+    if(!jsonFields || jsonFields === ""){
+        if(config.testingData){console.log('Empty query from public users req');}
+        return res.status(404).send({ status: 'failed', message: 'I cannot query emptiness.'});
+    }else{
+        User.find(query.username,query.fields,function(err,found){
+            if(err){
+                if(config.testingData){ console.log('Error getting field from user, public request.',err)};
+                return res.status(500).send({ error: err });
+            }
+            if(config.testingData){ console.log('Found from public request on fields',found)};
+            return res.status(200).send({ status: 'sucess', result: found});
+        });
+    }
+})
+//////En Public
+///////////////////////////////////////////////////////////////////////
+
 // get a single user from DB
 // router.get('/:id', function(req, res){
 //     if(checkId(req.params.id.toString())){

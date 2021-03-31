2901af1dd3a93f01315f2bedf4189136e17d3791
Saturno Mangieri <saturnob612@gmail.com>
2021-03-31 18:22:22 -0400

    more fixes

--- Nftcontroller.js
+++ Nftcontroller.js
@@ -38,13 +38,14 @@ const time = new Date();
 router.post('/createNFT', function(req,res){
     const token = req.headers['x-access-token'];
     const id = req.headers['id'];
+    console.log(`id:${id}`);
     if(!token) return res.status(404).send({ auth: false, message: 'No token provided!' });
     jwt.verify(token, config.secret, function(err, decoded){
         if(err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
         if(decoded){
             //search nfts first on hive
 
-            Nft.findOne({ nft_id: id},function(err,token){
+            Nft.findOne({ nft_id: Number(id)},function(err,token){
                 if(err){
                     if(config.testingData){
                         console.log('Error finding Nft',err);

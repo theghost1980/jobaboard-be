d69da7ec0875d66fbd89c11dc7708ea5f55200a3
Saturno Mangieri <saturnob612@gmail.com>
2021-05-11 19:38:58 -0400

    fixes

--- OrderController.js
+++ OrderController.js
@@ -443,20 +443,26 @@ router.post('/createMarketOrder', function(req,res){
                     if(config.testingData){ console.log('Error on multer', err)};
                     return res.status(500).send({ status: 'failed', message: err});
                 }
-                const item_type = req.body['item_type'];
-                if(item_type === "instance"){
-                    req.body['nft_instances'] = JSON.parse(req.body['nft_instances']);
-                }else{
-                    req.body['nft_instances'] = JSON.parse(req.body['nft_definitions']);
-                }
-                if(config.testingData){ console.log('About to create:',req.body)};
-                Order_Market.create(req.body, function(err, newOrder){
-                    if(err){
-                        if(config.testingData){ console.log('Error creating order:', err)};
-                        return res.status(500).send({ status: 'failed', message: err});
-                    }
-                    return res.status(200).send({ status: 'sucess', result: newOrder, message: `Order created. You can navigate to Marketplace > My Orders for review. Keep JABing!`})
-                }) ;
+                const data = req.body;
+                if(config.testingData){ console.log('About to create:', data)};
+                const item_type = data.item_type;
+                if(config.testingData){ 
+                    console.log('item_type:',item_type);
+                    console.log('nft_instances', data.nft_instances);
+                };
+                // if(item_type === "instance"){
+                //     data.nft_instances = JSON.parse(req.body['nft_instances']);
+                // }else{
+                //     req.body['nft_instances'] = JSON.parse(req.body['nft_definitions']);
+                // }
+                // if(config.testingData){ console.log('About to create:',req.body)};
+                // Order_Market.create(req.body, function(err, newOrder){
+                //     if(err){
+                //         if(config.testingData){ console.log('Error creating order:', err)};
+                //         return res.status(500).send({ status: 'failed', message: err});
+                //     }
+                //     return res.status(200).send({ status: 'sucess', result: newOrder, message: `Order created. You can navigate to Marketplace > My Orders for review. Keep JABing!`})
+                // }) ;
             });
         }else{
             return res.status(404).send({ auth: false, message: 'Failed to decode token.' });

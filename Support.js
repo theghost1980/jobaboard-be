37e384c43c39a48edaa9a2ea962c0a73aa16b3f4
Saturno Mangieri <saturnob612@gmail.com>
2021-05-21 16:38:24 -0400

    fixes

---
+++ Support.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var SupportSchema = new mongoose.Schema({
+    username: String,
+    usertype: String, //as admin, user.
+    ip_address: String,
+    ticket_type: String, //as general-support, specific-support, bug, suggestion, idea-improvement.
+    category_support: String,
+    solved: Boolean,
+    ref_id: String, //if needed the user may fill as tx_id or transaction id.
+    issue_description: String,
+    issue_note: String,
+    images: [String], //as the image uploaded to cloudinary, if needed.
+    createdAt: Date,
+    updatedAt: Date,
+});
+mongoose.model('Support', SupportSchema);
+
+module.exports = mongoose.model('Support');
